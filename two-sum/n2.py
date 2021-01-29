fcf4bfb9827f5ce31b2629049a203d5a4a91b844
Akash <akashpatel18041999@gmail.com>
2021-01-29 10:59:20 +0530

    done

--- two-sum/n2.py
+++ two-sum/n2.py
@@ -1,2 +1,14 @@
 def twosum(arr,target):
-    
+    for i in range(n):
+        for j in range(i,n):
+            if arr[i] + arr[j] == target:
+                return i,j
+
+
+if __name__=="__main__":
+
+    arr = [8,7,6,5]
+    target = 13
+    n = len(arr)
+
+    print(twosum(arr,target))
